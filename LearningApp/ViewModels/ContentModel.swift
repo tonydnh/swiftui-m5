a73b3a8248f6c2ae057ddf7d449706f710b714b2
Tony Dinh <tonydinh442@gmail.com>
2022-06-27 15:13:50 -0500

    - Rename content view - Organize files into folders - Add content model - Set content model as environment object

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Tony Dinh on 6/27/22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  LearningApp
 //
 //  Created by Tony Dinh on 6/27/22.
@@ -7,15 +7,20 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var model:ContentModel
+    
     var body: some View {
+        
         Text("Hello, world!")
             .padding()
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
