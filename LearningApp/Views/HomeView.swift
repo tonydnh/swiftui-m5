084ccf9ef8467969bbab6c7349b6fd5a4351d090
Tony Dinh <tonydinh442@gmail.com>
2022-06-27 21:30:36 -0500

    - Add home view row for each lesson and test card - Create the cards for the lessons and tests

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -13,8 +13,37 @@ struct HomeView: View {
     
     var body: some View {
         
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            VStack(alignment: .leading) {
+                
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    
+                    LazyVStack {
+                        
+                        ForEach(model.modules) { module in
+                            
+                            VStack(spacing: 20) {
+                                // MARK: Learning Card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
+                                // MARK: Test Card
+                                HomeViewRow(image: module.test.image, title: " \(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                                
+                            }
+
+                        }
+                        
+                    }
+                    .padding()
+                    
+                }
+            }
+            .navigationTitle("Get Started")
+        }
         
     }
 }
@@ -22,5 +51,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
