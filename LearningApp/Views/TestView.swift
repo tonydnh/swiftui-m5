efad95957fb28cb5136259f8a92fdcc3b6324eb1
Tony Dinh <tonydinh442@gmail.com>
2022-06-30 17:35:35 -0500

    - Add TestResultView - Show TestResultView in the TestView after the last question

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -137,8 +137,8 @@ struct TestView: View {
             
         }
         else {
-            // Test hasn't loaded yet
-            ProgressView()
+            // If current question is nil, we show the result view
+            TestResultView(numCorrect: numCorrect)
         }
         
     }
