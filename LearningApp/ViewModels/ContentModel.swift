33041e8d4b25e17a1fa6b6879361a080ec62178f
Tony Dinh <tonydinh442@gmail.com>
2022-06-30 21:42:34 -0500

    - Add the getRemoteData method to fetch data from a remote server - Call the getRemoteData in the init method of the ContentModel - Add bottom padding between the cards

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -34,9 +34,11 @@ class ContentModel: ObservableObject {
     var styleData:Data?
     
     init() {
-        
+        // Parse local included json data
         getLocalData()
         
+        // Download remote json file and parse data
+        getRemoteData()
     }
     
     // MARK: - Data methods
@@ -74,6 +76,52 @@ class ContentModel: ObservableObject {
             print("Couldn't parse style data")
         }
             
+        
+    }
+    
+    func getRemoteData() {
+        // String path
+        let urlString = "https://tonydnh.github.io/learningapp-data/data2.json"
+        
+        // Create a url object
+        let url = URL(string: urlString)
+        
+        guard url != nil else {
+            // Couldn't create url
+            return
+        }
+        
+        // Create a URLRequest object
+        let request = URLRequest(url: url!)
+        
+        // Get the session and kick off the task
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            // Check if there's an error
+            guard error == nil else {
+                // There was an error
+                return
+            }
+            
+            do {
+                // Create json decoder
+                let decoder = JSONDecoder()
+                
+                // Decode
+                let modules = try decoder.decode([Module].self, from: data!)
+                
+                // Append parsed modules into modules property
+                self.modules += modules
+            }
+            catch {
+                // Couldn't parse json
+            }
+            
+        }
+        
+        // Kick off data task
+        dataTask.resume()
         
     }
     
